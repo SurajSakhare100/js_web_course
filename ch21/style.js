c654095239ebcd7ffe181646e8e252a797f92501
suraj sakhare <sakharesuraj10@gmail.com>
2023-11-29 22:17:40 +0530

    ch21 added

---
+++ ch21/style.js
@@ -0,0 +1,20 @@
+let random1=Math.floor(Math.random()*3);
+let random2=Math.floor(Math.random()*3);
+let random3=Math.floor(Math.random()*3);
+
+let Adjective={
+    0:"Crazy",
+    1:"Amazing",
+    2:"Fire"
+};
+let Shop_name={
+    0:"Engine",
+    1:"Foods",
+    2:"Garments"
+};
+let Another_name={
+    0:"Bros",
+    1:"Limited",
+    2:"Hub"
+};
+console.log(Adjective[random1] +" "+Shop_name[random2]+" "+Another_name[random3]);
